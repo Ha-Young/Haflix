56846ef3f35692b2d22a35467e9b0a08679e500c
hychoi <hayeong28@naver.com>
2020-04-08 00:58:26 +0900

    make header component using react composition

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import Router from "./Router";
+import Header from "./Header";
 
 class App extends Component {
   render() {
     return (
-    <>
-      <Router />
-    </>
+      <>
+        <Header />
+        <Router />
+      </>
     );
   }
 }
--- src/Components/Router.js
+++ src/Components/Router.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {HashRouter as Router, Route} from "react-router-dom";
+import {BrowserRouter as Router, Route} from "react-router-dom";
 import Home from "../Routes/Home"
 import TV from "../Routes/TV"
 import Search from "../Routes/Search"
@@ -7,7 +7,8 @@ import Search from "../Routes/Search"
 export default () => (
     <Router>
             <Route path="/" exact component={Home} />
-            <Route path="/tv" exact component={TV} />
-            <Route path="/search" exact component={Search} />
+            <Route path="/tv" component={TV} />
+            <Route path="/tv/popular" render={() => <h1>Popular</h1>} />
+            <Route path="/search" component={Search} />
     </Router>
 );
