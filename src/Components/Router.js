55cbc6fe346966ad4f8ee01a342b7046ece2d6ac
hychoi <hayeong28@naver.com>
2020-04-08 01:16:50 +0900

    react router switch

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -1,14 +1,21 @@
 import React from "react";
-import {BrowserRouter as Router, Route} from "react-router-dom";
-import Home from "../Routes/Home"
-import TV from "../Routes/TV"
-import Search from "../Routes/Search"
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
+import Home from "../Routes/Home";
+import TV from "../Routes/TV";
+import Search from "../Routes/Search";
 
 export default () => (
-    <Router>
-            <Route path="/" exact component={Home} />
-            <Route path="/tv" component={TV} />
-            <Route path="/tv/popular" render={() => <h1>Popular</h1>} />
-            <Route path="/search" component={Search} />
-    </Router>
+  <Router>
+    <Switch>
+      <Route path="/" exact component={Home} />
+      <Route path="/tv" exact component={TV} />
+      <Route path="/search" component={Search} />
+      <Redirect from="*" to="/" />
+    </Switch>
+  </Router>
 );
