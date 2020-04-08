acf19f0c285d47ab88a9c57a65ce4b8c3fac6911
hychoi <hayeong28@naver.com>
2020-04-09 01:29:56 +0900

    complete writing api.js so beautiful clean code with arrow func, axios

--- README.md
+++ README.md
@@ -13,8 +13,10 @@ Clone Netflix (Movie Discovery App.)
 
 - [x] Now Playing (Movie)
 - [x] Upcoming (Movie)
-- [x] Airing Today (TV)
-- [x] Top Rated (Movie / TV)
-- [x] Popular (Movie / TV) 
-- [ ] TV Show Detail
-- [ ] Movie Detail
+- [x] Airing Today (TV Show)
+- [x] Top Rated (Movie / TV Show)
+- [x] Popular (Movie / TV Show) 
+- [x] TV Show Detail
+- [x] Movie Detail
+- [x] Search Movie
+- [x] Search TV Show
--- src/api.js
+++ src/api.js
@@ -8,21 +8,41 @@ const api = axios.create({
 api.interceptors.request.use((config) => {
   config.params = {
     api_key: "1f596200a866eee0095aa8edc88d23c0",
-    language: "ko-ko",
+    language: "ko-Kr",
     ...config.params,
   };
   return config;
 });
 
 export const movieAPI = {
-    nowPlaying: () => api.get("movie/now_playing"),
-    upcoming: () => api.get("movie/upcoming"),
-    topRated: () => api.get("movie/top_rated"),
-    popular: () => api.get("movie/popular")
-}
+  nowPlaying: () => api.get("movie/now_playing"),
+  upcoming: () => api.get("movie/upcoming"),
+  topRated: () => api.get("movie/top_rated"),
+  popular: () => api.get("movie/popular"),
+  detail: (movie_id) => api.get(`movie/${movie_id}`, {
+      params: {
+          append_to_response: "videos"
+      }
+  }),
+  search: (term) => api.get(`search/movie`, {
+      params: {
+          query: encodeURIComponent(term)
+      }
+  })
+};
 
 export const tvAPI = {
-    airingToday: () => api.get("tv/airing_today"),
-    top_rated: () => api.get("tv/top_rated"),
-    popular: () => api.get("tv/popular")
-}
+  airingToday: () => api.get("tv/airing_today"),
+  top_rated: () => api.get("tv/top_rated"),
+  popular: () => api.get("tv/popular"),
+  detail: (tv_id) => api.get(`tv/${tv_id}`, {
+      params: {
+          append_to_response: "videos"
+      }
+  }),
+  search: (term) => api.get(`search/tv`, {
+      params: {
+          query: encodeURIComponent(term)
+      }
+  })
+};
