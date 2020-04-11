227d62640d07dcd20f1b1d8205ce72ce53cb08e5
hychoi <hayeong28@naver.com>
2020-04-11 17:28:11 +0900

    HomeContainer, TVConatiner get datas by using api

--- src/Routes/Home/HomeContainer.js
+++ src/Routes/Home/HomeContainer.js
@@ -1,5 +1,6 @@
 import React from "react";
 import HomePresenter from "./HomePresenter";
+import { movieAPI } from "../../api";
 
 export default class extends React.Component {
     state = {
@@ -10,8 +11,36 @@ export default class extends React.Component {
         loading: true
     };
 
+    async componentDidMount() {
+        try {
+          const {
+            data: { results: nowPlaying }
+          } = await movieAPI.nowPlaying();
+          const {
+            data: { results: upcoming }
+          } = await movieAPI.upcoming();
+          const {
+            data: { results: popular }
+          } = await movieAPI.popular();
+          this.setState({
+            nowPlaying,
+            upcoming,
+            popular
+          });
+        } catch {
+          this.setState({
+            error: "Can't find movie information."
+          });
+        } finally {
+          this.setState({
+            loading: false
+          });
+        }
+    }
+
     render() {
         const { nowPlaying, upcoming, popular, error, loading } = this.state;
+
         return (
             <HomePresenter
                 nowPlaying={nowPlaying}
--- src/Routes/TV/TVContainer.js
+++ src/Routes/TV/TVContainer.js
@@ -1,25 +1,48 @@
 import React from "react";
 import TVPresenter from "./TVPresenter";
+import { tvAPI } from "../../api";
 
-export default class extends React.Component{
-    state = {
-        topRated: null,
-        popular: null,
-        airingToday: null,
-        loading: true,
-        error: null
-      };
-    
-    render() {
-        const {topRated, popular, airingToday, loading, error } = this.state;
-        return (
-            <TVPresenter
-                topRated={topRated}
-                popular={popular}
-                airingTday={airingToday}
-                loading={loading}
-                error={error}
-            />
-        )
+export default class extends React.Component {
+  state = {
+    topRated: null,
+    popular: null,
+    airingToday: null,
+    loading: true,
+    error: null,
+  };
+
+  async componentDidMount() {
+    try {
+      const {
+        data: { results: topRated },
+      } = await tvAPI.top_rated();
+      const {
+        data: { results: popular },
+      } = await tvAPI.popular();
+      const {
+        data: { results: airingToday },
+      } = await tvAPI.airingToday();
+      this.setState({ topRated, popular, airingToday });
+    } catch {
+      this.setState({
+        error: "Can't find TV information.",
+      });
+    } finally {
+      this.setState({ loading: false });
     }
+  }
+
+  render() {
+    const { topRated, popular, airingToday, loading, error } = this.state;
+    console.log(this.state);
+    return (
+      <TVPresenter
+        topRated={topRated}
+        popular={popular}
+        airingTday={airingToday}
+        loading={loading}
+        error={error}
+      />
+    );
+  }
 }
