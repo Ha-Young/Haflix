0e377cc281c2e1ff33c58dcad397b0b92ed30bcf
hychoi <hayeong28@naver.com>
2020-04-16 01:29:17 +0900

    message.js

--- src/Routes/TV/TVPresenter.js
+++ src/Routes/TV/TVPresenter.js
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 import styled from "styled-components";
 import Section from "../../Components/Section";
 import Loader from "../../Components/Loader";
+import Message from "../../Components/Message";
 
 const Container = styled.div`
   padding: 0px 20px;
@@ -15,25 +16,26 @@ const TVPresenter = ({ topRated, popular, airingToday, loading, error }) =>
     <Container>
       {topRated && topRated.length > 0 && (
         <Section title="Top Rated Shows">
-          {topRated.map(show => (
+          {topRated.map((show) => (
             <span key={show.id}>{show.name}</span>
           ))}
         </Section>
       )}
       {popular && popular.length > 0 && (
         <Section title="Popular Shows">
-          {popular.map(show => (
+          {popular.map((show) => (
             <span key={show.id}>{show.name}</span>
           ))}
         </Section>
       )}
       {airingToday && airingToday.length > 0 && (
         <Section title="Airing Today">
-          {airingToday.map(show => (
+          {airingToday.map((show) => (
             <span key={show.id}>{show.name}</span>
           ))}
         </Section>
       )}
+      {error && <Message color="#e74c3c" text={error} />}
     </Container>
   );
 
