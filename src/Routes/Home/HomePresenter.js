323a606bbf9d1909cd004e9528cc4b3461ba9b53
hychoi <hayeong28@naver.com>
2020-04-16 01:16:26 +0900

    add section.js and homepresenter show data

--- src/Routes/Home/HomePresenter.js
+++ src/Routes/Home/HomePresenter.js
@@ -1,1 +1,39 @@
-export default () => "Home";
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+import Section from "../../Components/Section";
+
+const Container = styled.div`
+  padding: 0px 10px;
+`;
+
+const HomePresenter = ({ nowPlaying, popular, upcoming, loading, error }) =>
+  loading ? null : (
+    <Container>
+      {nowPlaying && nowPlaying.length > 0 && (
+        <Section title="Now Playing">
+          {nowPlaying.map(movie => movie.title)}
+        </Section>
+      )}
+      {upcoming && upcoming.length > 0 && (
+        <Section title="Upcoming Movies">
+          {upcoming.map(movie => movie.title)}
+        </Section>
+      )}
+      {popular && popular.length > 0 && (
+        <Section title="Popular Movies">
+          {popular.map(movie => movie.title)}
+        </Section>
+      )}
+    </Container>
+  );
+
+HomePresenter.propTypes = {
+  nowPlaying: PropTypes.array,
+  upcoming: PropTypes.array,
+  popular: PropTypes.array,
+  error: PropTypes.string,
+  loading: PropTypes.bool.isRequired,
+};
+
+export default HomePresenter;
